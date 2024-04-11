966a90c258396eacee7f634c806548385c5e6102
Sh1bari <vova_krasnov_2004@mail.ru>
2024-04-11 19:34:11 +0300

    Initial commit

---
+++ src/main/java/com/example/zuulproxy/Configuration.java
@@ -0,0 +1,17 @@
+package com.example.zuulproxy;
+
+import lombok.*;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/api/**")
+                        .uri("http://localhost:8080/api"))
+                .build();
+    }
+}
