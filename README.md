786e192338c405db95d1d0d721587ef9bf5f2dec
Sh1bari <vova_krasnov_2004@mail.ru>
2024-04-14 20:39:06 +0300

    done

---
+++ README.md
@@ -0,0 +1,3 @@
+http://localhost:8111/admin-ui/applications
+http://localhost:8111/eureka-ui
+http://localhost:8111/api/auth/swagger-ui/index.html#/
--- src/main/java/com/example/zuulproxy/Configuration.java
+++ src/main/java/com/example/zuulproxy/Configuration.java
@@ -4,6 +4,9 @@ import lombok.*;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration{
@@ -11,12 +14,32 @@ public class Configuration{
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("spring_boot_eureka", r -> r.path("/eureka-ui/**")
-                        .filters(f -> f.stripPrefix(1))
                         .uri("lb://EUREKA-SERVER/"))
                 .route("spring_boot_eureka", r -> r.path("/eureka/**")
                         .uri("lb://EUREKA-SERVER/"))
                 .route("spring_boot_admin", r -> r.path("/admin-ui/**")
                         .uri("lb://MAIN-SERVER/admin-ui"))
+
+                .route("auth_server_swagger1", r -> r.path("/api/auth/swagger-ui/**")
+                        .filters(f -> f.stripPrefix(2))
+                        .uri("lb://AUTH-SERVER/"))
+                .route("auth_server_swagger2", r -> r.path("/api/auth/api/auth/v3/api-docs/**")
+                        .filters(f -> f.rewritePath("/api/auth/api/auth/v3/api-docs/(?<segment>.*)", "/api/auth/v3/api-docs/${segment}"))
+                        .uri("lb://AUTH-SERVER/"))
+                .route("auth_server", r -> r.path("/api/auth/**")
+                        .uri("lb://AUTH-SERVER/api/auth/"))
+
+                .route("main_server_swagger1", r -> r.path("/api/main/swagger-ui/**")
+                        .filters(f -> f.stripPrefix(2))
+                        .uri("lb://MAIN-SERVER/"))
+                .route("main_server_swagger2", r -> r.path("/api/main/api/main/v3/api-docs/swagger-config")
+                        .filters(f -> f.rewritePath("/api/main/api/main/v3/api-docs/swagger-config", "/api/main/v3/api-docs/swagger-config"))
+                        .uri("lb://MAIN-SERVER/"))
+                .route("main_server_swagger3", r -> r.path("/api/main/api/main/v3/api-docs")
+                        .filters(f -> f.stripPrefix(2))
+                        .uri("lb://MAIN-SERVER/api/main/v3/api-docs"))
+                .route("main_server", r -> r.path("/api/main/**")
+                        .uri("lb://MAIN-SERVER/api/main/"))
                 .build();
     }
 
