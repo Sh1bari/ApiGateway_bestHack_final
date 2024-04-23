6d7f006a76fa99e67f5e904e7ca91169df431e17
Sh1bari <vova_krasnov_2004@mail.ru>
2024-04-23 21:55:53 +0300

    done

--- src/main/java/com/example/zuulproxy/Configuration.java
+++ src/main/java/com/example/zuulproxy/Configuration.java
@@ -1,6 +1,7 @@
 package com.example.zuulproxy;
 
 import lombok.*;
+import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
